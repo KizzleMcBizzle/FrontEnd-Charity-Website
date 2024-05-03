8b8fd15507aed6efc57daf0efb5f87a6be52fc84
KizzleMcBizzle <omarelhussein03@gmail.com>
2024-05-03 20:35:56 +0300

    Update AdminNav.js

--- src/AdminNav.js
+++ src/AdminNav.js
@@ -278,15 +278,7 @@ export default function Example() {
                             </Popover.Group>
 
                             <div className="ml-auto flex items-center">
-                                <div className="hidden lg:flex lg:flex-1 lg:items-center lg:justify-end lg:space-x-6">
-                                    <a href="#" className="text-sm font-medium text-gray-700 hover:text-gray-800">
-                                        Sign in
-                                    </a>
-                                    <span className="h-6 w-px bg-gray-200" aria-hidden="true"/>
-                                    <a href="#" className="text-sm font-medium text-gray-700 hover:text-gray-800">
-                                        Create account
-                                    </a>
-                                </div>
+                                
 
                                 {/* Search */}
 
