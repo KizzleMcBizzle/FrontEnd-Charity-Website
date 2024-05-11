f2d3dbc4e0bd97af4719a354f98ac3f4bc79c467
seifdiea <seifdiea2003@gmail.com>
2024-05-11 18:20:43 +0300

    Fixed Teaching

--- src/TeachingList.js
+++ src/TeachingList.js
@@ -12,7 +12,7 @@ const TeachingList = ({ teachings }) => {
         <div className="teaching-list">
             {currTeachings.map(teaching => (
                 <div className="bg-gray-100 p-6 my-5 border-b border-gray-200 text-left shadow-md hover:shadow-lg rounded-lg transition-all duration-200 ease-in-out" key={teaching.id}>
-                      <Link to={`/donor/teachings/${teaching.id}`} className="no-underline">
+                      <Link to={`/donor/teaching/${teaching.id}`} className="no-underline">
                           <h2 className="mt-2 text-center text-2xl leading-9 tracking-tight text-gray-800 font-semibold hover:text-custom-green">{teaching.title}</h2>
                       </Link>
                 </div>
