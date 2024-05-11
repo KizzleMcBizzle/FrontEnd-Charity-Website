7d15ae1140866548fd779adeeabe498b69b2eac6
4mr-Nour <amrnour2003@gmail.com>
2024-05-11 21:45:02 +0300

    updated donate button location for books and food

--- src/ViewBookDetails.js
+++ src/ViewBookDetails.js
@@ -83,15 +83,16 @@ const ViewBookDetails = () => {
                             </div>
                         </dl>
                     </div>
-                    <div className="buttons mt-4 px-4 py-5 sm:px-6">
-               
-                         <button onClick={''}
-                            className="ml-3 inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md shadow-sm text-white bg-red-600 hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500 transition duration-300 ease-in-out transform hover:-translate-y-0.5 hover:scale-105">Donate
-                         </button>
-                    </div>
+                   
                 </div>
                 
                 )}
+                 <div className="buttons mt-4 px-4 py-5 sm:px-6">
+               
+               <button onClick={''}
+                  className="ml-3 inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md shadow-sm text-white bg-red-600 hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500 transition duration-300 ease-in-out transform hover:-translate-y-0.5 hover:scale-105">Donate
+               </button>
+          </div>
      </div>
     )}
     
