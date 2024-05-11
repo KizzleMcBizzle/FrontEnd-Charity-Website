90088424729596042744e542fea14738bbca624a
4mr-Nour <amrnour2003@gmail.com>
2024-05-11 21:42:02 +0300

    updated books and food DBs and their corresponding view details

--- src/ViewFoodDetails.js
+++ src/ViewFoodDetails.js
@@ -41,6 +41,14 @@ const ViewFoodDetails = () => {
                                     {food.type}
                                 </dd>
                             </div>
+                            <div className="py-3 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                                <dt className="text-sm font-medium text-gray-500">
+                                    Required Quantity
+                                </dt>
+                                <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+                                    {food.quantity_required}
+                                </dd>
+                            </div>
                         </dl>
                     </div>
                     <div className="buttons mt-4 px-4 py-5 sm:px-6">
