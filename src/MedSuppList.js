c2e474987d75037308ee23cf6b955c4d56dce678
seifdiea <seifdiea2003@gmail.com>
2024-05-11 18:22:09 +0300

    Fixed MedSuppList

--- src/MedSuppList.js
+++ src/MedSuppList.js
@@ -12,7 +12,7 @@ const MedSuppList = ({ medSupps }) => {
       <div className="medSupp-list">
           {currMedSupps.map(medSupp => (
               <div className="bg-gray-100 p-6 my-5 border-b border-gray-200 text-left shadow-md hover:shadow-lg rounded-lg transition-all duration-200 ease-in-out" key={medSupp.id}>
-                    <Link to={`/donor/medicalSupp/${medSupp.id}`} className="no-underline">
+                    <Link to={`/donor/medSupp/${medSupp.id}`} className="no-underline">
                         <h2 className="mt-2 text-center text-2xl leading-9 tracking-tight text-gray-800 font-semibold hover:text-custom-green">{ medSupp.name }</h2>
                     </Link>
               </div>
