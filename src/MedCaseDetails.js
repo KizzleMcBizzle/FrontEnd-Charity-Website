1c768c13e0549c502793ab64323853ea4bf5ccb8
seifdiea <seifdiea2003@gmail.com>
2024-05-12 05:29:04 +0300

    Pro-Bono Details and routing.

--- src/MedCaseDetails.js
+++ src/MedCaseDetails.js
@@ -4,7 +4,7 @@ import { useAuth } from "./AuthProvider";
 
 const MedCaseDetails = () => {
   const { id } = useParams();
-  const { data: medCase, error, isPending } = useFetch('http://localhost:4000/medCase/' + id);
+  const { data: teach, error, isPending } = useFetch('http://localhost:4000/medCase/' + id);
   const navigate = useNavigate();
   const user = useAuth();
   console.log(user);
@@ -15,73 +15,137 @@ const MedCaseDetails = () => {
   }
 
   const handleClick = () => {
-    alert(`You have fulfilled the request succesfully.`);
-    navigate("/");
+    setTimeout(()=>{
+      alert(`You have fulfilled the request succesfully.`);
+    navigate("/donor");
+
+    }, 1000)
+    
   }
 
   return (
-    <div className="donor-details bg-white shadow overflow-hidden sm:rounded-lg mx-4 my-4 p-4">
-      { isPending && <div>Loading...</div> }
-      { error && <div>{ error }</div> }
-      { medCase && (
+    <div className="organization-details bg-white shadow overflow-hidden sm:rounded-lg mx-4 my-4 p-4">
+    {isPending && <div>Loading...</div>}
+    {error && <div>{error}</div>}
+    {teach && (<>
         <div className="bg-white overflow-hidden shadow rounded-lg border">
-          <div className="px-4 py-5 sm:px-6">
-            <h2 className="text-lg leading-6 font-medium text-gray-900">Medical Case Details</h2>
-            <p className="mt-1 max-w-2xl text-sm text-gray-500">Details of the medical case.</p>
-          </div>
-          <div className="border-t border-gray-200 px-4 py-5 sm:p-0">
-            <dl className="sm:divide-y sm:divide-gray-200">
-              <div className="py-3 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-                <dt className="text-sm font-medium text-gray-500">Patient Name</dt>
-                <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">{ medCase.patientName }</dd>
-              </div>
-              <div className="py-3 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-                <dt className="text-sm font-medium text-gray-500">Age</dt>
-                <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">{ medCase.age }</dd>
-              </div>
-              <div className="py-3 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-                <dt className="text-sm font-medium text-gray-500">Gender</dt>
-                <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">{ medCase.gender }</dd>
-              </div>
-              <div className="py-3 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-                <dt className="text-sm font-medium text-gray-500">Weight</dt>
-                <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">{ medCase.weight }</dd>
-              </div>
-              <div className="py-3 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-                <dt className="text-sm font-medium text-gray-500">Location</dt>
-                <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2"><a href={medCase.location}>{medCase.location}</a></dd>
-              </div>
-              <div className="py-3 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-                <dt className="text-sm font-medium text-gray-500">Address</dt>
-                <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">{ medCase.address }</dd>
-              </div>
-              <div className="py-3 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-                <dt className="text-sm font-medium text-gray-500">Organization Name</dt>
-                <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">{ medCase.organizationName }</dd>
-              </div>
-              <div className="py-3 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-                <dt className="text-sm font-medium text-gray-500">Medical Specialty</dt>
-                <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">{ medCase.medicalSpecialty }</dd>
-              </div>
-              <div className="py-3 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-                <dt className="text-sm font-medium text-gray-500">Governorate</dt>
-                <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">{ medCase.governorate }</dd>
-              </div>
-              <div className="py-3 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-                <dt className="text-sm font-medium text-gray-500">Area</dt>
-                <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">{ medCase.area }</dd>
-              </div>
-            </dl>
-          </div>
+            <div className="px-4 py-5 sm:px-6">
+                <h3 className="text-lg leading-6 font-medium text-gray-900">
+                    Pro-Bono Medical Service
+                </h3>   
+            </div>
+            <div className="border-t border-gray-200 px-4 py-5 sm:p-0">
+                        <dl className="sm:divide-y sm:divide-gray-200">
+                            <div className="py-3 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                                <dt className="text-sm font-medium text-gray-500">
+                                    Patient Name
+                                </dt>
+                                <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+                                    {teach.patientName}
+                                </dd>
+                            </div>
+                            <div className="py-3 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                                <dt className="text-sm font-medium text-gray-500">
+                                    Age
+                                </dt>
+                                <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+                                    {teach.age}
+                                </dd>
+                            </div>
+                            <div className="py-3 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                                <dt className="text-sm font-medium text-gray-500">
+                                    Gender
+                                </dt>
+                                <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+                                    {teach.gender}
+                                </dd>
+                            </div>
+                            <div className="py-3 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                                <dt className="text-sm font-medium text-gray-500">
+                                    Weight
+                                </dt>
+                                <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+                                    {teach.weight}
+                                </dd>
+                            </div>
+                            <div className="py-3 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                                <dt className="text-sm font-medium text-gray-500">
+                                    Address
+                                </dt>
+                                <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+                                    {teach.address}
+                                </dd>
+                            </div>
+                            <div className="py-3 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+    <dt className="text-sm font-medium text-gray-500">
+        Area
+    </dt>
+    <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+        {teach.area}
+    </dd>
+</div>
+<div className="py-3 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+    <dt className="text-sm font-medium text-gray-500">
+        Governorate
+    </dt>
+    <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+        {teach.governorate}
+    </dd>
+</div>
+<div className="py-3 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+    <dt className="text-sm font-medium text-gray-500">
+        Organization Name
+    </dt>
+    <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+        {teach.organizationName}
+    </dd>
+</div>
+<div className="py-3 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+    <dt className="text-sm font-medium text-gray-500">
+        Medical Specialty
+    </dt>
+    <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+        {teach.medicalSpecialty}
+    </dd>
+</div>
+
+
+                    <div className="py-3 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                        <dt className="text-sm font-medium text-gray-500">
+                            Google Marker
+                        </dt>
+                        <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+                            <a href={teach.googleMapsUrl} className="mb-3">Google Maps</a>
+                            <iframe
+                                width="600"
+                                height="450"
+                                frameBorder="0"
+                                style={{border: 0}}
+                                src="https://www.google.com/maps/embed/v1/place?key=AIzaSyDAEp4t2VGfhtTwzdOlhpHEs-7v8N8iG7w&q=29.987350, 31.438084"
+                                allowFullScreen=""
+                                aria-hidden="false"
+                                tabIndex="0"
+                            ></iframe>
+                        </dd>
+                    </div>
+                </dl>
+            </div>
+
+
+            
         </div>
-      )}
-      <div className="buttons mt-4 px-4 py-5 sm:px-6">
-       {flag && <button onClick={handleClick}
-                className="inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md shadow-sm text-white bg-green-600 hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500 transition duration-300 ease-in-out transform hover:-translate-y-0.5 hover:scale-105">Fulfill The Request
+        {flag && <button onClick={handleClick}
+                                    className="ml-3 inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md shadow-sm text-white bg-green-600 hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500 transition duration-300 ease-in-out transform hover:-translate-y-0.5 hover:scale-105">Fulfill
         </button>}
-      </div>
-    </div>
-  );
+
+
+
+        </>
+    )}
+</div>
+);
 }
+ 
+
 
 export default MedCaseDetails;
--- src/TeachingDetails.js
+++ src/TeachingDetails.js
@@ -1,36 +1,116 @@
-import {  useParams } from "react-router-dom";
+import {  useNavigate, useParams } from "react-router-dom";
 import useFetch from "./useFetch";
 import {useAuth} from './AuthProvider';
 
 const TeachingDetails = () => {
 
+  const navigate = useNavigate();
+
   const user = useAuth();
   const { id } = useParams();
-  const { data: teaching, error, isPending } = useFetch('http://localhost:4000/teaching/' + id);
+  const { data: teach, error, isPending } = useFetch('http://localhost:4000/teaching/' + id);
   let flag = false;
 
   if( user.role && user.role.toLowerCase() === 'teacher'){
     flag = true;
   }
 
+  const handleClick = () => {
+    setTimeout(()=>{
+      alert(`You have fulfilled the request succesfully.`);
+    navigate("/donor");
+
+    }, 1000)
+    
+  }
 
   return (
-    <div className="teaching-details">
-      { isPending && <div>Loading...</div> }
-      { error && <div>{ error }</div> }
-      { teaching && (
-        <article>
-          <h2>{ teaching.title }</h2>
-          <p>Governorate:{teaching.governorate}</p>
-          <p>Area: {teaching.area}</p>
-          <p>Number of Students: { teaching.number_of_students }</p>
-          <div>Address: { teaching.address }</div>
-          <a href={teaching.google_map_marker}>Location</a>
-          <p>Subject Name: {teaching.subjects}</p>
-        </article>
-      )}
-    </div>
-  );
+    <div className="organization-details bg-white shadow overflow-hidden sm:rounded-lg mx-4 my-4 p-4">
+    {isPending && <div>Loading...</div>}
+    {error && <div>{error}</div>}
+    {teach && (<>
+        <div className="bg-white overflow-hidden shadow rounded-lg border">
+            <div className="px-4 py-5 sm:px-6">
+                <h3 className="text-lg leading-6 font-medium text-gray-900">
+                    Pro-Bono Teaching Service
+                </h3>   
+            </div>
+            <div className="border-t border-gray-200 px-4 py-5 sm:p-0">
+                        <dl className="sm:divide-y sm:divide-gray-200">
+                            <div className="py-3 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                                <dt className="text-sm font-medium text-gray-500">
+                                    Title
+                                </dt>
+                                <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+                                    {teach.title}
+                                </dd>
+                            </div>
+                            <div className="py-3 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                                <dt className="text-sm font-medium text-gray-500">
+                                    Area
+                                </dt>
+                                <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+                                    {teach.area}
+                                </dd>
+                            </div>
+                            <div className="py-3 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                                <dt className="text-sm font-medium text-gray-500">
+                                    Governorate
+                                </dt>
+                                <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+                                    {teach.governorate}
+                                </dd>
+                            </div>
+                            <div className="py-3 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                                <dt className="text-sm font-medium text-gray-500">
+                                    Number of Students
+                                </dt>
+                                <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+                                    {teach.number_of_students}
+                                </dd>
+                            </div>
+                            <div className="py-3 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                                <dt className="text-sm font-medium text-gray-500">
+                                    Address
+                                </dt>
+                                <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+                                    {teach.address}
+                                </dd>
+                            </div>
+                    <div className="py-3 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                        <dt className="text-sm font-medium text-gray-500">
+                            Google Marker
+                        </dt>
+                        <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+                            <a href={teach.googleMapsUrl} className="mb-3">Google Maps</a>
+                            <iframe
+                                width="600"
+                                height="450"
+                                frameBorder="0"
+                                style={{border: 0}}
+                                src="https://www.google.com/maps/embed/v1/place?key=AIzaSyDAEp4t2VGfhtTwzdOlhpHEs-7v8N8iG7w&q=29.987350, 31.438084"
+                                allowFullScreen=""
+                                aria-hidden="false"
+                                tabIndex="0"
+                            ></iframe>
+                        </dd>
+                    </div>
+                </dl>
+            </div>
+
+
+            
+        </div>
+        {flag && <button onClick={handleClick}
+                                    className="ml-3 inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md shadow-sm text-white bg-green-600 hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500 transition duration-300 ease-in-out transform hover:-translate-y-0.5 hover:scale-105">Fulfill
+        </button>}
+
+
+
+        </>
+    )}
+</div>
+);
 }
  
 export default TeachingDetails;
