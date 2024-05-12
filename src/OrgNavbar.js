9f19d3c3cc16adf6c1be9a183e156b15336dce60
KizzleMcBizzle <omarelhussein03@gmail.com>
2024-05-12 20:33:50 +0300

    Updated
    Added 2 buttons for OrgNavBar
    
    Made the Google Marker show the address of the pin instead of markers

--- src/OrgNavbar.js
+++ src/OrgNavbar.js
@@ -43,8 +43,10 @@ const navigation = {
         },
     ],
     pages: [
-        { name: 'About Us', href: '/admin/' },
+        { name: 'About Us', href: '/org/' },
         { name: 'Contact', href: '#' },
+        { name: 'Create Post', href: '/org/create-post' },
+        { name: 'View Posts', href: '/org/view-posts' }
     ],
 }
 
@@ -193,7 +195,7 @@ export default function Example() {
 
                             {/* Logo */}
                             <div className="ml-4 flex lg:ml-0">
-                                <Link to="/admin/">
+                                <Link to="/org/">
                                     <span className="sr-only">HopeUnity</span>
                                     <img
                                         className="h-10 w-auto"
@@ -306,16 +308,32 @@ export default function Example() {
                                 {/* Search */}
 
                                 {/* Notifications */}
-                                <div className="ml-4 flow-root lg:ml-6">
-                                    <button
-                                        type="button"
-                                        className="relative rounded-full border-custom-green border-2 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-custom-green"
-                                    >
-                                        <span className="absolute -inset-1.5"/>
-                                        <span className="sr-only">View notifications</span>
-                                        <BellIcon className="h-6 w-6" aria-hidden="true"/>
-                                    </button>
-                                </div>
+                                <Popover className="relative">
+                                    {({ open }) => (
+                                        <>
+                                            <Popover.Button className="rounded-full flex text-sm focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-custom-green focus:ring-white">
+                                                <span className="sr-only">View notifications</span>
+                                                <BellIcon className="h-8 w-8 rounded-full" aria-hidden="true" />
+                                            </Popover.Button>
+                                            <Transition
+                                                as={Fragment}
+                                                enter="transition ease-out duration-200"
+                                                enterFrom="transform opacity-0 scale-95"
+                                                enterTo="transform opacity-100 scale-100"
+                                                leave="transition ease-in duration-75"
+                                                leaveFrom="transform opacity-100 scale-100"
+                                                leaveTo="transform opacity-0 scale-95"
+                                            >
+                                                <Popover.Panel className="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5 focus:outline-none">
+                                                    <div className="py-1">
+                                                        <Link to="/org/post" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">A donor has fulfilled one of your posts!</Link>
+                                                        <Link to="/org/post" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">A donor has fulfilled one of your posts!</Link>
+                                                    </div>
+                                                </Popover.Panel>
+                                            </Transition>
+                                        </>
+                                    )}
+                                </Popover>
 
                                 {/* Profile dropdown */}
                                 <Popover className="relative ml-4">
