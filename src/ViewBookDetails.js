9fad2278e605503cbbe7c1566e98648786aface3
KizzleMcBizzle <omarelhussein03@gmail.com>
2024-05-12 03:17:34 +0300

    Added Quantity Slider

--- src/ViewBookDetails.js
+++ src/ViewBookDetails.js
@@ -13,7 +13,7 @@ const ViewBookDetails = () => {
     const navigate = useNavigate();
 
     const [modalIsOpen, setModalIsOpen] = useState(false);
-
+    const [quantity, setQuantity] = useState(1);
     const openModal = () => {
         setModalIsOpen(true);
     }
@@ -97,10 +97,23 @@ const ViewBookDetails = () => {
                                 </dl>
                             </div>
                         </div>
-                        <div className="buttons mt-4 px-4 py-5 sm:px-6">
-                            <button onClick={''}
-                                    className="ml-3 inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md shadow-sm text-white bg-custom-green hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500 transition duration-300 ease-in-out transform hover:-translate-y-0.5 hover:scale-105">Donate
-                            </button>
+                        <div>
+                            <h4 className="font-medium text-gray-900">Quantity to be donated</h4>
+                            <div className="relative">
+                                <input
+                                    type="range"
+                                    min="1"
+                                    max="20"
+                                    value={quantity}
+                                    onChange={(e) => setQuantity(e.target.value)}
+                                    className="green-thumb-slider slider appearance-none w-64 h-1 bg-gradient-to-r from-green-400 to-blue-500 rounded-full shadow-inner cursor-pointer"
+                                    style={{'WebkitAppearance': 'none'}}
+                                />
+                                <div
+                                    className="absolute left-0 top-0 h-1 w-full rounded-full"
+                                    style={{width: `${quantity * 5}%`}}></div>
+                            </div>
+                            <p className="text-gray-900">{quantity}</p>
                         </div>
                     </div>
                     <div className="w-2/5 pl-4 flex flex-col">
